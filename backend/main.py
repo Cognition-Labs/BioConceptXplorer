18c751ee70af699de112dc686a39f0a09c9a32fc
Shrey Joshi <shreyjoshi2004@gmail.com>
2023-06-09 21:11:08 -0500

    complete free var search optimized

--- backend/main.py
+++ backend/main.py
@@ -1,67 +1,194 @@
-from itertools import islice
+import sys
+import time
 import time
 import os
 import pandas as pd
-import shelve
-import json
 import numpy as np
-import pickle
 from tqdm import tqdm
 import modal
+from sklearn.metrics.pairwise import cosine_similarity
+from core.init import (
+    init_all_if_needed,
+)
 
+
+# init all the data then load it
+init_all_if_needed()
+from core.init import (
+    model,
+    BERT_embeddings,
+    BERT_sentences,
+    BCV_list,
+    BCV_values,
+    BCV_descriptions,
+)
 
 DATA_PATH = "/root/embeddings"
 image = modal.Image.debian_slim().pip_install_from_requirements("requirements.txt")
 
 mounts = [
-    modal.Mount.from_local_file(".env", remote_path="/root/.env"),
-    modal.Mount.from_local_directory("embeddings", remote_path=DATA_PATH),
+    modal.Mount.from_local_file(".env", remote_path=os.path.join(DATA_PATH, ".env")),
+    modal.Mount.from_local_dir("embeddings", remote_path="/root/embeddings"),
 ]
 
-stub = modal.Stub(name="bioconceptexplorer", mounts=mounts, image=image)
+stub = modal.Stub(name="BioConceptVecXplorer", mounts=mounts, image=image)
 
-def load_embeddings_data():
-    print("Cold start\n- loading embeddings from shared volume cache...")
-    with open(os.path.join(DATA_PATH, "concept_glove.json")) as json_file:
-        concept_glove = json.load(json_file)
-        print("loaded embeddings file")
-        concept_list = list(concept_glove.keys())
-        print("loaded concept list")
-        embedding_values = np.array(list(concept_glove.values()), dtype=np.float32)
-        print("loaded embedding values")
-    return concept_glove, concept_list, embedding_values
 
-def load_bert_embeddings():
-    sentence_embeddings = np.load(os.path.join(DATA_PATH, "description_embeddings.npy"))
-    return sentence_embeddings
-
-def load_bert_sentences():
-    with open(os.path.join(DATA_PATH, "sentences.txt")) as f:
-        sentences = f.readlines()
-    return sentences
-
-concept_glove, concept_list, embedding_values = load_embeddings_data()
-sentence_embeddings = load_bert_embeddings()
-sentences = load_bert_sentences()
-
-@stub.function()
+@stub.function(cpu=2, memory=2048, container_idle_timeout=300, keep_warm=1)
 @modal.web_endpoint(method="GET")
 def bert_query(query: str, top_k: int = 10):
-    similarities = np.dot(sentence_embeddings, sentence_embeddings[concept_list.index(query)])
-    top_k_idx = np.argsort(similarities)[::-1][:top_k]
-    return [sentences[idx] for idx in top_k_idx]
+    query_vector = model.encode([query])
+    sims = cosine_similarity(query_vector, BERT_embeddings)[0]
+    indices = np.argsort(sims)[::-1][:top_k]
+    options = {
+        BERT_sentences[i].strip("\n"): float(sims[j]) for j, i in enumerate(indices)
+    }
+    return options
+
 
 @stub.function()
 @modal.web_endpoint(method="GET")
 def compute_expression(expression: list, top_k: int = 10) -> dict:
     pass
 
-@stub.function()
-@modal.web_endpoint(method="GET")
-def free_var_search(query: str, top_k: int = 10):
-    pass
 
-@stub.function()
+def get_BCV_vector(query: str):
+    query_index = np.where(BCV_list == query)[0][0]
+    query_vector = BCV_values[query_index]
+    return query_vector
+
+
+def map_BCV_to_description(unmapped: str):
+    mapped = BCV_descriptions[unmapped] if unmapped in BCV_descriptions else "N/A"
+    if type(mapped) == list:
+        mapped = " aka ".join(mapped)
+    return mapped
+
+
+"""
+Given a single concept query, performs a free variable search using equation of the form
+Q + B - C = D
+where Q is the query and B, C, D are free variables.
+"""
+
+
+@stub.function(cpu=2, memory=2048, container_idle_timeout=300)
 @modal.web_endpoint(method="GET")
-def heartbeat():
-    return "OK"
+def free_var_search(query: str, n: int = 1_000, sim_threshold: float = 0.75):
+    print(f"----- Performing free variable search for {query}... -----")
+    start_freevar = time.time()
+
+    # Get the query vector from BCV embeddings
+    q = query.strip("\n")
+    q_vector = get_BCV_vector(q)
+    q_mapped = map_BCV_to_description(q)
+
+    # Perform the free variable search
+    # - generate n random equations (B, C) pairs by picking indices from 0-->len(BCV_list)
+    start = time.time()
+    print(f"- Generating {n} equation samples...", end=" ", flush=True)
+    equation_indices = np.random.choice(len(BCV_list), size=(n, 2))
+    print(f"Done in {time.time() - start} seconds")
+
+    start = time.time()
+    print(f"- Computing B, C, D vectors...", end=" ", flush=True)
+
+    # use indices to get B, C vectors
+    b_vectors = BCV_values[equation_indices[:, 0]]
+    c_vectors = BCV_values[equation_indices[:, 1]]
+
+    # compute D vector
+    d_vectors = q_vector.repeat(n).reshape(n, -1) + b_vectors - c_vectors
+    print(f"Done in {time.time() - start} seconds")
+
+    # compute cosine similarity between D and all BCVs
+    start = time.time()
+    print(f"- Computing cosine similarities...", end=" ", flush=True)
+    sims = cosine_similarity(d_vectors, BCV_values)
+    print(f"Done in {time.time() - start} seconds")
+
+    # get top 4 results
+    start = time.time()
+    print(f"- Indexing results...", end=" ", flush=True)
+    K = 4  # worst case Q, B, C all in top 3 in which case we pick the 4th
+    indices = np.argpartition(sims, -K, axis=1)[:, -K:].copy()
+
+    # get indices of b, c, q vectors to remove from top 4 (don't want D = one of B, C, Q)
+    b_indices = equation_indices[:, 0]
+    c_indices = equation_indices[:, 1]
+    q_index = np.where(BCV_list == query)[0][0]
+    print(f"Done in {time.time() - start} seconds")
+
+    results = []
+    start = time.time()
+    print(f"- Mapping results to build dataframe...", end=" ", flush=True)
+    for i, (b_idx, c_idx) in tqdm(enumerate(equation_indices), total=n):
+        b = BCV_list[b_idx]
+        c = BCV_list[c_idx]
+
+        # argpartition doesn't sort the top k, so we sort first
+        indices[i] = indices[i][np.argsort(sims[i][indices[i]])][::-1]
+        # then calc d index = remove b, c, q indices from top 5 and get the first one
+        d_idx = indices[i][
+            ~np.isin(
+                indices[i], [b_indices[i], c_indices[i], q_index], assume_unique=True
+            )
+        ][0]
+        d = BCV_list[d_idx]
+        similarity = sims[i][d_idx]
+
+        if similarity < sim_threshold:
+            continue
+
+        # map to descriptions and get equations/similarity
+        b_mapped = map_BCV_to_description(b)
+        c_mapped = map_BCV_to_description(c)
+        d_mapped = map_BCV_to_description(d)
+        eq = f"{query} + {b} - {c} = {d}"
+        eq_mapped = f"{q_mapped} + {b_mapped} - {c_mapped} = {d_mapped}"
+
+        results.append(
+            (
+                eq,
+                q,
+                b,
+                c,
+                d,
+                eq_mapped,
+                q_mapped,
+                b_mapped,
+                c_mapped,
+                d_mapped,
+                similarity,
+            )
+        )
+
+    df = pd.DataFrame(
+        results,
+        columns=[
+            "Equation",
+            "Q",
+            "B",
+            "C",
+            "D",
+            "Equation (Mapped)",
+            "Q (Mapped)",
+            "B (Mapped)",
+            "C (Mapped)",
+            "D (Mapped)",
+            "Similarity",
+        ],
+    )
+    print(f"Done in {time.time() - start} seconds")
+
+    # sort by similarity
+    df = df.sort_values(by="Similarity", ascending=False).reset_index(drop=True)
+
+    print(
+        f"----- Finished Free Variable Search in {time.time() - start_freevar} seconds -----"
+    )
+    return df.to_dict(orient="records")
+
+
+if __name__ == "__main__":
+    print(free_var_search("Gene_6853"), file=open("output.csv", "w+"))
