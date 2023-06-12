22ab4a06f4e5505cc4fe2b101f539a3bf9416085
Daniel George <degtrdg@github.com>
2023-06-12 00:31:03 -0500

    Added checkbox for gpt and more context

--- streamlit/main.py
+++ streamlit/main.py
@@ -12,15 +12,18 @@ def process_input(user_input: str) -> List[str]:
         options_str = ["No similar concepts found. Please try again."]
     return options_str
 
-def get_free_var_search(query, sim_threshold):
+def get_free_var_search(query, sim_threshold, use_gpt=False):
     base_url = 'https://shreyj1729--bioconceptvecxplorer-free-var-search.modal.run'
     n_samples = 100
-    use_gpt = 'gpt-4'
+    if use_gpt:
+        gpt = 'gpt-4'
+    else:
+        gpt = 'none'
     params = {
         'query': query,
         'n': n_samples,
         'sim_threshold': sim_threshold,
-        'use_gpt': use_gpt
+        'use_gpt':gpt 
     }
 
     try:
@@ -50,9 +53,13 @@ user_input = st.text_input("Enter a concept:", value=st.session_state['user_inpu
 if st.button('Process Concept') or st.session_state['user_input'] != '':
     st.session_state['user_input'] = user_input
     st.session_state['options'] = process_input(user_input)
+    st.session_state['gpt'] = False
 
     if 'options' in st.session_state:
         selected_option = st.selectbox("Select a similar concept:", st.session_state['options'])
+        st.write("The option you select will be used to search for concepts in the following form:")
+        st.write('[selected concept] + X - Y = Z')
+        st.write('where X, Y, and Z are free variables in the latent space.')
         if st.button('Confirm Concept'):
             st.session_state['selected_option'] = selected_option
 
@@ -63,10 +70,12 @@ if st.button('Process Concept') or st.session_state['user_input'] != '':
             st.write(f"You selected: {extracted_string}")
 
             threshold = st.selectbox("Select a threshold:", [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0])
+            gpt = st.checkbox("Use GPT-4")
             if st.button('Submit Threshold'):
                 st.session_state['threshold'] = threshold
-
-                df = get_free_var_search(extracted_string, st.session_state['threshold'])
+                st.session_state['gpt'] = gpt
+                st.write("Please wait while we process your request...")
+                df = get_free_var_search(extracted_string, st.session_state['threshold'], st.session_state['gpt'])
                 if df is not None:
                     st.write("Here are the results:")
                     st.download_button(
